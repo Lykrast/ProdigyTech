ff174bf9ba48dc1be8cb3c5a54c27b72741dfd87
Lykrast <aymericpm@gmail.com>
2017-11-12 10:08:11 +0100

    Explosion Furnace now shows tooltips

--- src/main/java/lykrast/prodigytech/client/gui/GuiExplosionFurnace.java
+++ src/main/java/lykrast/prodigytech/client/gui/GuiExplosionFurnace.java
@@ -38,5 +38,13 @@ public class GuiExplosionFurnace extends GuiContainer {
         this.fontRenderer.drawString(s, this.xSize / 2 - this.fontRenderer.getStringWidth(s) / 2, 6, 4210752);
         this.fontRenderer.drawString(this.playerInventory.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
     }
+    
+    @Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks)
+	{
+		this.drawDefaultBackground();
+		super.drawScreen(mouseX, mouseY, partialTicks);
+		this.renderHoveredToolTip(mouseX, mouseY);
+	}
 
 }

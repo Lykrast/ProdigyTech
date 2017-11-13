d86b7febd20445081570447b518e8530089fd696
Lykrast <aymericpm@gmail.com>
2017-11-13 20:07:41 +0100

    Should now load on all 1.12 subversions

--- src/main/java/lykrast/prodigytech/core/ProdigyTech.java
+++ src/main/java/lykrast/prodigytech/core/ProdigyTech.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ProdigyTech.MODID, name = ProdigyTech.NAME, version = ProdigyTech.VERSION)
+@Mod(modid = ProdigyTech.MODID, name = ProdigyTech.NAME, version = ProdigyTech.VERSION, acceptedMinecraftVersions = "[1.12, 1.13)")
 public class ProdigyTech
 {
     public static final String MODID = "prodigytech";

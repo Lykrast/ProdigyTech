315d7b292605122c5ed85439dd93ce0d092edfaf
Lykrast <aymericpm@gmail.com>
2017-11-16 19:47:34 +0100

    Added animated arrow to Incinerator JEI

--- src/main/java/lykrast/prodigytech/common/compat/jei/IncineratorCategory.java
+++ src/main/java/lykrast/prodigytech/common/compat/jei/IncineratorCategory.java
@@ -1,15 +1,30 @@
 package lykrast.prodigytech.common.compat.jei;
 
+import lykrast.prodigytech.common.util.Config;
 import mezz.jei.api.IGuiHelper;
+import mezz.jei.api.gui.IDrawableAnimated;
+import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.config.Constants;
+import net.minecraft.client.Minecraft;
 
 public class IncineratorCategory extends ProdigyCategory<IncineratorWrapper> {
 	public static final String UID = "ptincinerator";
+	private final IDrawableAnimated arrow;
 
 	public IncineratorCategory(IGuiHelper guiHelper) {
 		super(guiHelper, guiHelper.createDrawable(ProdigyTechJEI.GUI, 0, 36, 82, 26, 0, 20, 0, 0), UID);
+		
+		IDrawableStatic arrowDrawable = guiHelper.createDrawable(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17);
+		this.arrow = guiHelper.createAnimatedDrawable(arrowDrawable, Config.incineratorProcessTime, IDrawableAnimated.StartDirection.LEFT, false);
+	}
+	
+	@Override
+	public void drawExtras(Minecraft minecraft)
+	{
+		arrow.draw(minecraft, 24, 5);
 	}
 
 	@Override

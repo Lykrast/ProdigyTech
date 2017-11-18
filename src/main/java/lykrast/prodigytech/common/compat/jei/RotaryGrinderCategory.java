9561c428d7a6fdb6442d2ec7a766541c980c3114
Lykrast <aymericpm@gmail.com>
2017-11-18 09:52:56 +0100

    Grinder recipes with different processing time now show up correctly in JEI

--- src/main/java/lykrast/prodigytech/common/compat/jei/RotaryGrinderCategory.java
+++ src/main/java/lykrast/prodigytech/common/compat/jei/RotaryGrinderCategory.java
@@ -5,32 +5,17 @@ import java.util.List;
 
 import lykrast.prodigytech.common.recipe.RotaryGrinderManager;
 import lykrast.prodigytech.common.recipe.RotaryGrinderManager.RotaryGrinderRecipe;
-import lykrast.prodigytech.common.util.Config;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IModRegistry;
-import mezz.jei.api.gui.IDrawableAnimated;
-import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.config.Constants;
-import net.minecraft.client.Minecraft;
 
 public class RotaryGrinderCategory extends ProdigyCategory<RotaryGrinderWrapper> {
 	public static final String UID = "ptgrinder";
-	private final IDrawableAnimated arrow;
 
 	public RotaryGrinderCategory(IGuiHelper guiHelper) {
 		super(guiHelper, guiHelper.createDrawable(ProdigyTechJEI.GUI, 0, 36, 82, 26), UID);
-		
-		IDrawableStatic arrowDrawable = guiHelper.createDrawable(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17);
-		this.arrow = guiHelper.createAnimatedDrawable(arrowDrawable, Config.rotaryGrinderProcessTime, IDrawableAnimated.StartDirection.LEFT, false);
-	}
-	
-	@Override
-	public void drawExtras(Minecraft minecraft)
-	{
-		arrow.draw(minecraft, 24, 5);
 	}
 
 	@Override
@@ -45,11 +30,12 @@ public class RotaryGrinderCategory extends ProdigyCategory<RotaryGrinderWrapper>
 
 	public static void registerRecipes(IModRegistry registry)
 	{
+		IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
 		List<RotaryGrinderWrapper> list = new ArrayList<>();
 		
 		for (RotaryGrinderRecipe recipe : RotaryGrinderManager.RECIPES)
 		{
-			list.add(new RotaryGrinderWrapper(recipe));
+			list.add(new RotaryGrinderWrapper(recipe, guiHelper));
 		}
 		
 		registry.addRecipes(list, UID);
--- src/main/java/lykrast/prodigytech/common/compat/jei/RotaryGrinderWrapper.java
+++ src/main/java/lykrast/prodigytech/common/compat/jei/RotaryGrinderWrapper.java
@@ -5,16 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 import lykrast.prodigytech.common.recipe.RotaryGrinderManager.RotaryGrinderRecipe;
+import mezz.jei.api.IGuiHelper;
+import mezz.jei.api.gui.IDrawableAnimated;
+import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
+import mezz.jei.config.Constants;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class RotaryGrinderWrapper implements IRecipeWrapper {
 	private ItemStack out;
 	private List<List<ItemStack>> in;
+	private final IDrawableAnimated arrow;
 	
-	public RotaryGrinderWrapper(RotaryGrinderRecipe recipe)
+	public RotaryGrinderWrapper(RotaryGrinderRecipe recipe, IGuiHelper guiHelper)
 	{
 		List<ItemStack> inputs = new ArrayList<>();
 		
@@ -28,12 +34,20 @@ public class RotaryGrinderWrapper implements IRecipeWrapper {
 		
 		in = Collections.singletonList(inputs);
 		out = recipe.getOutput();
+		
+		IDrawableStatic arrowDrawable = guiHelper.createDrawable(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17);
+		arrow = guiHelper.createAnimatedDrawable(arrowDrawable, recipe.getTimeTicks(), IDrawableAnimated.StartDirection.LEFT, false);
 	}
 
 	@Override
 	public void getIngredients(IIngredients ingredients) {
 		ingredients.setInputLists(ItemStack.class, in);
 		ingredients.setOutput(ItemStack.class, out);
+	}
+	
+	@Override
+	public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+		arrow.draw(minecraft, 24, 5);
 	}
 
 }

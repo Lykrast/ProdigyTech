6b4f2f3a8b85eb6139105d5afcd2d46824eb2f8b
Lykrast <aymericpm@gmail.com>
2018-04-26 10:25:27 +0200

    Added Guide-API as a dependency

--- src/main/java/lykrast/prodigytech/core/ProdigyTech.java
+++ src/main/java/lykrast/prodigytech/core/ProdigyTech.java
@@ -10,12 +10,17 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ProdigyTech.MODID, name = ProdigyTech.NAME, version = ProdigyTech.VERSION, acceptedMinecraftVersions = "[1.12, 1.13)")
+@Mod(modid = ProdigyTech.MODID, 
+	name = ProdigyTech.NAME, 
+	version = ProdigyTech.VERSION, 
+	acceptedMinecraftVersions = "[1.12, 1.13)",
+	dependencies = ProdigyTech.DEPENDENCIES)
 public class ProdigyTech
 {
-    public static final String MODID = "prodigytech";
-    public static final String NAME = "Prodigy Tech";
-    public static final String VERSION = "0.0.1";
+    public static final String MODID = "prodigytech",
+    		NAME = "Prodigy Tech",
+    		VERSION = "0.0.1",
+    		DEPENDENCIES = "required-after:guideapi;";
     
 	@Instance
 	public static ProdigyTech instance;
